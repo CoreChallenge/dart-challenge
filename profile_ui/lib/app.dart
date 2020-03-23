ae8a6c038da3832d4d38effb3af76bf1192bcb37
CoreChallenge <easybuilderall@gmail.com>
2020-03-23 15:49:24 +0700

    Added: some ui component on the layout

--- profile_ui/lib/app.dart
+++ profile_ui/lib/app.dart
@@ -13,7 +13,50 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: Text("Profile UI"),
         ),
-      ),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.purple[300],
+                Colors.purple[50]
+              ]),
+          ),
+          child: Column(children: <Widget>[
+              CircleAvatar(
+                radius: 80,
+                backgroundImage: AssetImage("images/man-default-profile.jpg") ,
+              ),
+              
+                ListTile(
+                  title: Center(child: Text("Nick Flutter", style: TextStyle(fontSize: 20),),) ,
+                  subtitle: Center(child: Text("Sr. Web and Mobile Application Developer", style: TextStyle(fontSize: 15),),) 
+                ),
+
+                Row( 
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                      RaisedButton(
+                      onPressed: (){
+
+                    },
+                      child: Text("Message"),),
+
+                    RaisedButton(
+                      onPressed: (){
+
+                    },
+                      child: Text("Hire Me"),),
+                  ],
+                ),
+                
+                ListTile(title: Text("About Me", style: TextStyle(fontSize: 16),),
+                subtitle: Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", style: TextStyle(fontSize: 15),),)
+              ]
+          ,),
+        )
+      ),// 
     );
   }
 }
