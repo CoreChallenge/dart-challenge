b92deabe378f03a5b9e5e90975d97c2a19c841c9
CoreChallenge <easybuilderall@gmail.com>
2020-03-24 01:04:12 +0700

    Completed: All section for layout

--- base_layout/lib/main.dart
+++ base_layout/lib/main.dart
@@ -1,37 +1,76 @@
-
-
 import 'package:flutter/material.dart';
 
-main(){
+main() {
   runApp(App());
 }
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     
+    /* Description and Rating Section */
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32),
       child: Row(
         children: <Widget>[
           Expanded(
+            /* 1 */
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
+                /* 2 */
                 Container(
                   padding: const EdgeInsets.only(bottom: 8),
-                  child: Text("Oeschinen Lake Campground", style: TextStyle(fontWeight: FontWeight.bold),),),
-                  Text("Kandersteg, Switzerland", style: TextStyle(color: Colors.grey[500]),)
+                  child: Text(
+                    "Oeschinen Lake Campground",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  "Kandersteg, Switzerland",
+                  style: TextStyle(color: Colors.grey[500]),
+                )
               ],
-            ),),
-            Icon(Icons.star,
-            color: Colors.red[500],
             ),
-            Text("41"),
+          ),
+          /* 3 */
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text("41"),
         ],
       ),
     );
+
+    /* Button Section */
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      )
+    );
+
+    /* Text Section */
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
 
     return MaterialApp(
       title: "Flutter Layout 101",
@@ -39,18 +78,43 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: Text("Flutter Layout 101"),
         ),
-      body:ListView(
-        children: <Widget>[
-          Image.asset(
-            "images/lake.jpg", 
-            width: 600,
-            height: 240,
-            fit: BoxFit.cover,),
-            titleSection
-        ],
+        body: ListView(
+          children: <Widget>[
+            Image.asset(
+              "images/lake.jpg",
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection
+          ],
         ),
       ),
-      );
+    );
   }
 
+  Column _buildButtonColumn(Color color, IconData icon, String label){
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        )
+      ],
+    );
+  }
 }
+
+
